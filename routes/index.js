4d0e6b7b210911240be6002bdcd00646483013b4
Preston Richey <prichey@barkleyus.com>
2017-08-13 00:26:20 -0500

    Movie rating check to index request so it'll be up to date

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,13 @@ const low = require('lowdb');
 const approvalDb = low('db/approval.json');
 const movieDb = low('db/movies.json');
 
-const rating = parseInt(approvalDb.get('rating').value().val);
-
 function getRandomMovieWithRating(rating) {
   const movies = movieDb.get(rating).value();
   return movies[Math.floor(Math.random() * movies.length)];
 }
 
 router.get('/', function(req, res) {
+  const rating = parseInt(approvalDb.get('rating').value().val);
   const movie = getRandomMovieWithRating(rating);
 
   res.render('index', {
