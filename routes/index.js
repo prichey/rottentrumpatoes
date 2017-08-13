686f85cd9e98f012b6bbd0943ac656497a367cbc
Preston Richey <prichey@barkleyus.com>
2017-08-13 00:58:52 -0500

    Fix issues with non-integer rating

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,16 @@ const express = require('express');
 const router = express.Router();
 const low = require('lowdb');
 const approvalDb = low('db/approval.json');
-const movieDb = low('db/movies.json');
+const moviesDb = low('db/movies.json');
 
 function getRandomMovieWithRating(rating) {
-  const movies = movieDb.get(rating).value();
+  const movies = moviesDb.get(rating).value();
   return movies[Math.floor(Math.random() * movies.length)];
 }
 
 router.get('/', function(req, res) {
-  const rating = parseInt(approvalDb.get('rating').value().val);
+  const ratingObj = approvalDb.get('rating').value();
+  const rating = parseInt(ratingObj.val);
   const movie = getRandomMovieWithRating(rating);
 
   res.render('index', {
