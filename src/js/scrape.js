1e5f0cd6499591e2654b91e89e84aa9a8acf332b
Preston Richey <prichey@barkleyus.com>
2017-08-13 14:42:24 -0500

    Add jquery to scroll to bottom of log when appending

--- src/js/scrape.js
+++ src/js/scrape.js
@@ -3,6 +3,7 @@ const socket = io.socket;
 const moment = require('moment');
 
 const $log = $('ul.log');
+const $htmlBody = $('html, body');
 
 function formatDataLog(str) {
   return `<li><span class='date'>[${moment().format(
@@ -14,6 +15,7 @@ function run() {
   io.init();
   socket.on('new_msg', function(data) {
     $log.append(formatDataLog(data.msg));
+    $htmlBody.animate({ scrollTop: $(document).height() });
   });
   console.log('scrape');
 }
