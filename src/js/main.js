128c5d9d77fc26a88cdc738aa1373b094e1dc4ee
Preston Richey <prichey@barkleyus.com>
2017-09-19 22:05:20 -0500

    Hook up share

--- src/js/main.js
+++ src/js/main.js
@@ -11,49 +11,79 @@ const $whatButton = $('#what-link');
 const $closeSectionButton = $('.section-close');
 const $mainContent = $('.main-content');
 
+function closeActiveModals() {
+  $('.nav-sections li.active').removeClass('active');
+  $('.nav-links li.active').removeClass('active');
+  $mainContent.removeClass('nopacity');
+}
+
 function hookUpShareButtonClick() {
   $shareButton.on('click tap touch', e => {
     e.preventDefault();
+
     console.log('share');
+    const $shareLink = $(`[data-section-link='share']`);
+    const $shareSection = $(`[data-section='share']`);
+
+    if ($(`[data-section-link='share']`).hasClass('active') !== true) {
+      closeActiveModals();
+      $shareLink.addClass('active');
+      $shareSection.addClass('active');
+      $mainContent.addClass('nopacity');
+    } else {
+      closeActiveModals();
+    }
   });
 
   return;
 }
 
 function hookUpWhoButtonClick() {
-  $whoButton.on('click tap touch', e => {
+  $whoButton.on('click tap touch', function(e) {
     e.preventDefault();
+
     console.log('who');
-    $('.nav-links li.active').removeClass('active');
-    $(`[data-section-link='who']`).addClass('active');
-    $('.nav-sections li.active').removeClass('active');
-    $(`[data-section='who']`).addClass('active');
-    $mainContent.addClass('nopacity');
+    const $whoLink = $(`[data-section-link='who']`);
+    const $whoSection = $(`[data-section='who']`);
+
+    if ($(`[data-section-link='who']`).hasClass('active') !== true) {
+      closeActiveModals();
+      $whoLink.addClass('active');
+      $whoSection.addClass('active');
+      $mainContent.addClass('nopacity');
+    } else {
+      closeActiveModals();
+    }
   });
 
   return;
 }
 
 function hookUpWhatButtonClick() {
-  $whatButton.on('click tap touch', e => {
+  $whatButton.on('click tap touch', function(e) {
     e.preventDefault();
+
     console.log('what');
-    $('.nav-links li.active').removeClass('active');
-    $(`[data-section-link='what']`).addClass('active');
-    $('.nav-sections li.active').removeClass('active');
-    $(`[data-section='what']`).addClass('active');
-    $mainContent.addClass('nopacity');
+    const $whatLink = $(`[data-section-link='what']`);
+    const $whatSection = $(`[data-section='what']`);
+
+    if ($(`[data-section-link='what']`).hasClass('active') !== true) {
+      closeActiveModals();
+      $whatLink.addClass('active');
+      $whatSection.addClass('active');
+      $mainContent.addClass('nopacity');
+    } else {
+      closeActiveModals();
+    }
   });
 
   return;
 }
 
 function hookUpCloseButtonClick() {
-  $closeSectionButton.on('click tap touch', e => {
+  $closeSectionButton.on('click tap touch', function(e) {
     e.preventDefault();
-    $('.nav-sections li.active').removeClass('active');
-    $('.nav-links li.active').removeClass('active');
-    $mainContent.removeClass('nopacity');
+    closeActiveModals();
   });
 }
 
